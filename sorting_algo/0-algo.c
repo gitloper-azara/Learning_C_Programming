35a6ff31723d584cfad26a7b0a886e08971ae68a
Yushahu Yussif <yyushahu02@gmail.com>
2024-01-17 23:14:27 +0000

    insertion sort

---
+++ sorting_algo/0-algo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n)
+{
+    int i, j, key;
+
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printarray(int arr[], int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main(void)
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: \n");
+    printarray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printarray(arr, n);
+
+    return (0);
+}
